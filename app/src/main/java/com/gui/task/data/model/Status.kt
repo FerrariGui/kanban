c3c6edf6a92b7331a709f8ccdd405e43511c8373
Guilherme <guilhermeferrarimiranda@gmail.com>
2025-09-03 19:54:35 -0300

    kanban

---
+++ app/src/main/java/com/gui/task/data/model/Status.kt
@@ -0,0 +1,7 @@
+package com.gui.task.data.model
+
+enum class Status {
+    TODO,
+    DOING,
+    DONE
+}
---
+++ app/src/main/java/com/gui/task/ui/TodoFragment.kt
@@ -0,0 +1,82 @@
+package com.gui.task.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.gui.task.R
+import com.gui.task.data.model.Status
+import com.gui.task.data.model.Task
+import com.gui.task.databinding.FragmentTodoBinding
+import com.gui.task.ui.adapter.TaskAdapter
+
+
+class TodoFragment : Fragment() {
+
+    private var _binding: FragmentTodoBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var taskAdapter: TaskAdapter
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentTodoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListeners()
+        initRecyclerViewTask(getTask())
+    }
+
+    private fun initListeners(){
+        binding.floatingActionButton2.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_formTaskFragment)
+        }
+    }
+
+    private fun initRecyclerViewTask(taskList: List<Task>){
+        taskAdapter = TaskAdapter(requireContext(), taskList) {task, option -> optionSelected(task,option)}
+        binding.recyclerViewTask.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerViewTask.setHasFixedSize(true)
+        binding.recyclerViewTask.adapter = taskAdapter
+    }
+
+    private fun optionSelected(task:Task, option: Int){
+        when (option){
+            TaskAdapter.SELECT_REMOVER -> {
+                Toast.makeText(requireContext(), "Removendo ${task.description}", Toast.LENGTH_SHORT).show()
+            }
+            TaskAdapter.SELECT_EDIT -> {
+                Toast.makeText(requireContext(), "Editando ${task.description}", Toast.LENGTH_SHORT).show()
+            }
+            TaskAdapter.SELECT_DETAILS -> {
+                Toast.makeText(requireContext(), "Detalhes ${task.description}", Toast.LENGTH_SHORT).show()
+            }
+            TaskAdapter.SELECT_NEXT -> {
+                Toast.makeText(requireContext(), "Próximo", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun getTask() = listOf(
+        Task("0", "Integrar pagamento com cartão de crédito", Status.TODO),
+        Task("1", "Implementar sistema de recuperação de senha", Status.TODO),
+        Task("2", "Adicionar filtro de pesquisa avançada", Status.TODO),
+        Task("3", "Criar dashboard de estatísticas para o usuário", Status.TODO),
+        Task("4", "Habilitar login com autenticação em duas etapas", Status.TODO),
+    )
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
